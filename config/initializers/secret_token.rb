f800f314d20b9ad913c22d7ab584f1e94a5d00a6
Caleb Majo <calebmajo@yahoo.com>
2013-05-28 10:34:19 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swahilichef::Application.config.secret_token = '54310a4cf1b846f8a51d32b2417c2ee9efe23c971052f0ca95f6aba05a8ec9306d2531b4c590417593603755b42a6627a09ae5d715004247c5e85c975fb316a0'
